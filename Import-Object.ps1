1fd27bde3c64eeb41de8f21717be769de9c585de
Tore Groneng <tore.groneng@gmail.com>
2015-03-16 23:49:17 +0100

    refactor a bit

--- Import-Object.ps1
+++ Import-Object.ps1
@@ -15,33 +15,37 @@ Param(
     Write-Verbose -Message "$f -  Getting data"
     $db = Get-XMLdocument
 
-    if(-not ($KeyValue.ObjectType -ne "Person" -or $KeyValue.ObjectType -ne "Group"))
-    {
-        throw "Object type $($KeyValue.ObjectType) not supported"
-    }
-
-    if($KeyValue.Operation -ne "Update")
-    {
-        throw "Operation on objectID $($KeyValue.ObjectID) cannot complete, operation $($KeyValue.Operation) is not supported or null"
-    }
-
-    if(-not $KeyValue.ObjectName)
-    {
-        throw "ObjectName $($KeyValue.ObjectName) is not supported or null"
-    }
-
     [string]$ObjectID    = $KeyValue.ObjectID
     [string]$ObjectType  = $KeyValue.ObjectType
     [string]$ObjectName  = $KeyValue.ObjectName
     [string]$ObjectValue = $KeyValue.ObjectValue
+    [string]$Operation   = $KeyValue.Operation
+
+    if(-not ($ObjectType -ne "Person" -or $ObjectType -ne "Group"))
+    {
+        throw "Object type '$ObjectType' not supported"
+    }
+
+    if($Operation -ne "Update")
+    {
+        throw "'$Operation' operation on objectID '$ObjectID' cannot complete, the operation is not supported or null"
+    }
+
+    if(-not $ObjectName)
+    {
+        throw "ObjectName '$ObjectName' is not supported or null"
+    }
 
     [string]$xPath = "//$ObjectType[@ObjectID = '$ObjectID']"
-    Write-Verbose -Message "$f -  xpath = $xPath"
+    Write-Verbose -Message "$f -  xPath = $xPath"
 
     $obj = $db | Select-Xml -XPath $xPath
     Write-Verbose -Message "$F -  ObjectName = $ObjectName = $ObjectValue"
 
     $obj.Node.$ObjectName = $ObjectValue
 
-    Set-Variable -Name xml -Value $db.InnerXml -Scope Global    
+    Write-Verbose -Message "$f -  Updating global scope variable"
+    Set-Variable -Name xml -Value $db.InnerXml -Scope Global 
+    
+    Write-Verbose -Message "$f - END"   
 }
